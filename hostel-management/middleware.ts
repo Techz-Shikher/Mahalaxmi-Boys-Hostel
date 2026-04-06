2832d6cf0b10007885bc07d2b78a61dd20b3ce1d
Techz-Shikher <shikhersingh32286@gmail.com>
2026-04-06 14:32:47 +0530

    Fix: Prefix unused request parameter with underscore

--- hostel-management/middleware.ts
+++ hostel-management/middleware.ts
@@ -4,7 +4,7 @@
 
 import { type NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function middleware(_request: NextRequest) {
   return NextResponse.next();
 }
 
